dd1370a5ff72825b365eb8631b3a9dffa0355fa4
CAPN <nathan.gs20@gmail.com>
2020-12-09 12:08:02 -0600

    Passed!

--- 2020-09.py
+++ 2020-09.py
@@ -18,6 +18,9 @@ def test_b():
                     preamble=5
     ) == get_test_answer(part='b', fname=__file__)
 
+def test_actual_b():
+    assert answer_b(data, 25) == 13549369
+
 def generate_data_iterator(data_string):
     return tuple(int(d) for d in data_string.split('\n'))
 
@@ -32,12 +35,14 @@ def answer_a(input_data=data, preamble=5):
     return 0
 
 def answer_b(input_data=data, preamble=5):
+    ans_a = answer_a(input_data, preamble)
     data_iter = generate_data_iterator(input_data)
-    for line, d in enumerate(data_iter[preamble:]):
+    for line in range(len(data_iter[preamble:])):
         for start, end in combinations(range(0,preamble),2):
-            if d == sum(data_iter[line + start: line + end + 1]):
-                return data_iter[line + start] + data_iter[line + end + 1]
+            seq = data_iter[line + start: line + end]
+            if ans_a == sum(seq):
+                return min(seq) + max(seq)
     return 0
 
-submit(answer_a(data, 25), part='a')
-submit(answer_b(data, 25), part='b')
+# submit(answer_a(data, 25), part='a')
+# submit(answer_b(data, 25), part='b')
